72638b29bdd5050d2df15940fa8c4f3a5ce47c60
ramoneiggy <ramoneiggy@gmail.com>
2019-08-16 22:39:16 +0200

    Added simple test

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.Test;
+
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    public void isTrue() {
+        int num = 1;
+        assert num == 1;
+    }
+
+}
